76e3436abe3c947e7f97e00a3dbf2a32cbc18a3a
Faruk <so38ware@gmail.com>
2025-04-28 23:39:57 +0300

    Add RootLayout component and SplashScreen files with styles

---
+++ app/screens/side/SplashScreen/styles.ts
@@ -0,0 +1,32 @@
+/*
+    dark-mode: https://coolors.co/001219-005f73-0a9396-94d2bd-e9d8a6-ee9b00-ca6702-bb3e03-ae2012-9b2226
+    light-mode: https://coolors.co/f0f4f8-b5d5ea-32c1cd-c6f1e7-ffc300-faedcd-d7263d-ff9f1c-ff6b35-a91b23
+*/
+const Colors = {
+    dark: {
+      background: '#001219',         // Ana zemin
+      surface: '#005F73',             // Kartlar, inputlar
+      primary: '#0A9396',             // Ana aksiyon rengi
+      primaryLight: '#94D2BD',        // Açık aksiyon rengi
+      secondary: '#EE9B00',           // İkincil aksiyon (sarı-turuncu gibi)
+      secondaryLight: '#E9D8A6',      // Açık ikincil
+      error: '#AE2012',               // Hatalar
+      warning: '#CA6702',             // Uyarılar
+      info: '#BB3E03',                // Bilgilendirme (turuncu-kahve tonu)
+      danger: '#9B2226',              // Kritik hata (en koyu kırmızı)
+    },
+    light: {
+      background: '#F0F4F8',          // Ana zemin
+      surface: '#B5D5EA',             // Kartlar, inputlar
+      primary: '#32C1CD',             // Ana aksiyon rengi
+      primaryLight: '#C6F1E7',        // Açık aksiyon rengi
+      secondary: '#FFC300',           // İkincil aksiyon
+      secondaryLight: '#FAEDCD',      // Açık ikincil
+      error: '#D7263D',               // Hatalar
+      warning: '#FF9F1C',             // Uyarılar
+      info: '#FF6B35',                // Bilgilendirme
+      danger: '#A91B23',              // Kritik hata
+    },
+  };
+  
+export default Colors;  
